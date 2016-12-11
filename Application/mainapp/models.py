84ec98f25cc9327f285e8bfcb3c8189aacb60fd3
Ryorda Triaptahadi <triaptahadiryorda@yahoo.co.id>
2016-12-12 06:30:39 +0700

    update sqlite

--- Application/mainapp/models.py
+++ Application/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    author = models.CharField(max_length=30)
+    singer = models.CharField(max_length=30)
+    year = models.CharField(max_length=8)
+    genre = models.CharField(max_length=30)
+    lyric = models.TextField()
+	
+class EmotionSong(models.Model) :
+	song = models.ForeignKey(Song, on_delete=models.CASCADE)
+	anger = models.FloatField()
+	disgust = models.FloatField()
+	fear = models.FloatField()
+	sadness = models.FloatField()
+	happiness = models.FloatField()
+	surprise = models.FloatField()
